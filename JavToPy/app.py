6b9736480f2a938f27fd348ddc059029cd8c0dfa
LegitimateAMX <xiang.andre@gmail.com>
2024-10-12 15:20:32 -0700

    cleaned up negated lines and commented

--- JavToPy/app.py
+++ JavToPy/app.py
@@ -1,6 +1,6 @@
 from flask import Flask,render_template, request, jsonify
 import os
-import translators as ts
+import translators as ts #translator library
 
 
 app = Flask(__name__,template_folder="templates")
@@ -13,11 +13,7 @@ def hello():
 def process():
     data = request.get_json() # retrieve the data sent from JavaScript
     # process the data using Python code
-    #result = data['value'] * 2
-    result = ts.translate_text(data['value'])
-    #return jsonify(result=result) # return the result to JavaScript
-    print(data)
-    print(result)
-    return jsonify(result=result)
+    result = ts.translate_text(data['value']) #actual translation
+    return jsonify(result=result) #return in js
 if __name__ == '__main__':
     app.run(debug=True)
